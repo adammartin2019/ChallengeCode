fa12da4506f6412ec7d3aba50ddb3efc2eec3156
Adam Martin <adammartin@Adams-MacBook-Pro.local>
2021-01-10 12:02:12 -0500

    complete two sum solution

--- TwoSum_Python.py
+++ TwoSum_Python.py
@@ -47,4 +47,35 @@ twoSum([2,7,11,15], 9)
 #The best solution combines hash table insertion and checking the complement at the same time
 #resulting in O(n) runtime as well as space complexity
 
+def twoSum(nums, target):
+    indexList = []
+    hMap = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hMap:
+            indexList.append(i)
+            indexList.append(hMap.get(complement))
+            print(indexList)
+            return indexList
+        hMap.update({nums[i]:i})
     
+
+twoSum([2,7,11,15], 9)
+
+
+def TwoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+TwoSum([2,7,11,15], 9)
